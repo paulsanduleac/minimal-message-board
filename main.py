9102443728264378596a9d05978a27107bbf88ce
Thomas Langston <thomas.langston@mongodb.com>
2019-04-04 14:00:16 -0400

    Remove broswer version check

--- main.py
+++ main.py
@@ -44,10 +44,6 @@ html = """<!DOCTYPE html>
         </style>
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
         <h1>Message Board</h1>
         <p>Last {msg_count} messages left here</p>
 
